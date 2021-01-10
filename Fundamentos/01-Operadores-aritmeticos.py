dc13a943ca369ee0d235f9091f798aa2505c87e3
José Luis Ferrete <josel.ferrete@s-dos.es>
2021-01-10 13:33:18 +0100

    Operadores Aritméticos en Python

---
+++ Fundamentos/01-Operadores-aritmeticos.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+suma = a + b
+print("suma:", suma)
+resta = a - b
+print("resta:", resta)
+multi = a * b
+print("multiplicacion:", multi)
+division = a / b
+print("division:", division)
+modulo = a % b
+print("modulo:", modulo)
+exponente = a ** b
+print("exponente:", exponente)
