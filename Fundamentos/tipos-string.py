de69804584667c4331eec77baa349889b62fa28b
José Luis Ferrete <josel.ferrete@s-dos.es>
2021-01-10 12:53:26 +0100

    Tipos String (Cadena) en Python

---
+++ Fundamentos/tipos-string.py
@@ -0,0 +1,11 @@
+cadena = "aerosmith"
+print("Mi grupo favorito es: " + cadena)
+
+numero1 = "1"
+numero2 = "2"
+print("contacatenacion:" + numero1 + numero2)
+
+num1 = 1
+num2 = 2
+#print("operacion suma:" + str(num1 + num2))
+print("operacion suma:", num1 + num2)
